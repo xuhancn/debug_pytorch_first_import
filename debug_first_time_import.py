9d60d26f99578531984b4dc32b97494f5e3edf08
Xu Han <xu.han@outlook.com>
2025-12-15 14:12:24 +0800

    add code.

---
+++ debug_first_time_import.py
@@ -0,0 +1,9 @@
+import time
+
+start_time = time.time()
+
+import torch
+
+end_time = time.time()
+execution_time = end_time - start_time
+print(f"Code execution time: {execution_time:.4f} seconds")
