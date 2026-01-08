346110c0934a88c6f4a763bf35175fe73bbd8274
Xu Han <xu.han@outlook.com>
2026-01-08 15:48:58 +0800

    update code.

--- debug_first_time_import.py
+++ debug_first_time_import.py
@@ -1,9 +1,19 @@
 import time
+from viztracer import VizTracer
+
+tracer = VizTracer()
+tracer.start()
 
 start_time = time.time()
 
 import torch
 
 end_time = time.time()
+
+tracer.stop()
+tracer.save() # also takes output_file as an optional argument
+
 execution_time = end_time - start_time
 print(f"Code execution time: {execution_time:.4f} seconds")
+
+# ui.perfetto.dev for analysis.
